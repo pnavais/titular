6db0d3baa836d1131918435d0ae0664865369249
Pablo Navais <pnavais@gmail.com>
2025-05-26 19:17:39 +0200

    Added clear line functionality

--- src/transforms/line_handler.rs
+++ src/transforms/line_handler.rs
@@ -1,6 +1,8 @@
 use crate::context_manager::ContextManager;
 use crate::error::Result;
+use crate::term::TERM_SIZE;
 use crate::transforms::Transform;
+use console::Term;
 
 /// Handles line endings based on context flags
 pub struct LineHandler;
@@ -13,9 +15,18 @@ impl LineHandler {
 
 /// LineHandler is a transform that handles line manipulations based on context flags.
 /// For example, it can be used to skip the newline character at the end of the text.
+/// When clear is active, it will move to the beginning of the line and clear it.
 impl Transform for LineHandler {
     fn transform(&self, text: &str) -> Result<String> {
         let ctx = ContextManager::get().read()?;
+
+        if ctx.is_active("clear") {
+            let term = Term::stdout();
+            term.clear_line()?;
+            term.move_cursor_left(TERM_SIZE.get_term_width())?;
+            return Ok(text.to_string());
+        }
+
         Ok(format!(
             "{}{}",
             text,
